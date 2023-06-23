669390194f20c02663bc31050328dbddfdf101e2
brico1994 <b.rico.barrilero@gmail.com>
2023-06-23 17:58:53 +0200

    sveltestrap

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 import {loadBackendBRBv1} from "./backend/backend-v1.js";
+import { loadBackendBRBv2 } from "./backend/backend-v2.js";
+import { handler } from "./frontend/build/handler.js";
 
 var app = express();
 
@@ -9,11 +12,12 @@ app.use(express.json());
 
 var port = process.env.PORT || 12345;
 
+loadBackendBRBv1(app);
+loadBackendBRBv2(app);
 
+app.use(handler);
+app.use(cors);
 
 app.listen(port, () => {
-    console.log(`Server ready in port ${port}`);
-  });
-
-
-loadBackendBRBv1(app);
+  console.log(`Server ready in port ${port}`);
+});
