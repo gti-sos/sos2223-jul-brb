c294924632727535e72d665be696d77b4c9f59e1
brico1994 <b.rico.barrilero@gmail.com>
2023-06-27 19:22:55 +0200

    arreglo del page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,10 +7,14 @@
         Mi fuente de informacion analiza datos de turismo en Andalucia durante el 2022</li>
         <li><strong>Repositorio</strong>: <a href="https://github.com/gti-sos/SOS2223-jul-brb">gti-sos/SOS2223-jul-brb</a></li>
         <li><strong>URL</strong>: <a href="http://sos2223-jul-brb.appspot.com/">http://sos2223-jul-brb.appspot.com/ </a></li>
-        <li><strong>API</strong>: <a href="/api/v2/tourism">Estadisticas de turismo </a>   (creado por <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)</li>
+        <li><strong>APIs</strong>:</li> 
+            <ul>
+                <a href="/api/v1/tourism">Estadisticas de turismo V1 </a>   (creado por <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)
+                <a href="/api/v2/tourism">Estadisticas de turismo V2</a>   (creado por <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)
+            </ul>
         <li><strong>Documentacion</strong>:<ul>
-        <li><a href="http://sos2223-19.appspot.com/api/v1/tourism/docs">http://sos2223-19.appspot.com/api/v1/tourism/docs</a> (developed by <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)</li>
-        <li><a href="http://sos2223-19.appspot.com/api/v2/tourism/docs">http://sos2223-19.appspot.com/api/v2/tourism/docs</a> (developed by <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)</li>
+        <li><a href="http://sos2223-jul-brb.appspot.com/api/v1/tourism/docs">http://sos2223-jul-brb.appspot.com/api/v1/tourism/docs</a> (developed by <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)</li>
+        <li><a href="http://sos2223-jul-brb.appspot.com/api/v2/tourism/docs">http://sos2223-jul-brb.appspot.com/api/v2/tourism/docs</a> (developed by <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)</li>
         </ul>
         <li><strong>Frontend</strong>: <a href="/tourism">Estadisticas de turismo </a>   (creado por <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)  
         <li><strong>Backend</strong>: <a href="/api/v1/tourism">Estadisticas de turismo </a>   (creado por <a href="https://github.com/brico1994">Bruno Alvaro Rico Barrilero</a>)  
