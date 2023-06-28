431e01928d208da6d51adf175e7e53424d0ff1da
brico1994 <b.rico.barrilero@gmail.com>
2023-06-28 02:07:10 +0200

    visualizacion

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -2,5 +2,6 @@
     <div class="menuC">
         <a class="active" href="/"> Inicio </a>
         <a href="/tourism"> Estadisticas de turismo </a>
+        <a href="/graphs"> Graficos </a>
     </div>
 </nav>
