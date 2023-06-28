1eb34e1d1dbfcd7efcbb0469855722c83fef5f54
brico1994 <b.rico.barrilero@gmail.com>
2023-06-28 20:26:00 +0200

    test e2e

---
+++ e2e/tests-tourism.spec.js
@@ -0,0 +1,42 @@
+//@ts-check
+import { test, expect } from'@playwright/test';
+
+test('get occupation stats', async ({ page }) => {
+    await page.goto('https://sos2223-19.appspot.com/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Gráfico' }).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*graphs/);
+  });
+
+  /*test('get analytics', async ({ page }) => {
+    await page.goto('https://sos2223-19.appspot.com/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Grafo general' }).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*analytics/);
+  });
+
+  test('get uses and integrations', async ({ page }) => {
+    await page.goto('https://sos2223-19.appspot.com/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Usos y Integraciones' }).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*integrations/);
+  });
+
+  test('get about', async ({ page }) => {
+    await page.goto('https://sos2223-19.appspot.com/');
+  
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Sobre nosotros' }).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*about/);
+  });*/
