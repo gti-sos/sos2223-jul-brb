d7e9641580f9b2ef3200790a4e7d4a2d28f03ea4
brico1994 <b.rico.barrilero@gmail.com>
2023-06-30 21:17:33 +0200

    usos e integraciones

--- e2e/tests-tourism.spec.js
+++ e2e/tests-tourism.spec.js
@@ -11,15 +11,7 @@ test('get tourism stats', async ({ page }) => {
     await expect(page).toHaveURL(/.*graphs/);
   });
 
-  /*test('get analytics', async ({ page }) => {
-    await page.goto('https://sos2223-19.appspot.com/');
-  
-    // Click the get started link.
-    await page.getByRole('link', { name: 'Grafo general' }).click();
-  
-    // Expects the URL to contain intro.
-    await expect(page).toHaveURL(/.*analytics/);
-  });*/
+ 
 
   test('get uses and integrations', async ({ page }) => {
     await page.goto('https://sos2223-jul-brb.appspot.com/');
@@ -31,12 +23,4 @@ test('get tourism stats', async ({ page }) => {
     await expect(page).toHaveURL(/.*integrations/);
   });
 
-  /*test('get about', async ({ page }) => {
-    await page.goto('https://sos2223-19.appspot.com/');
-  
-    // Click the get started link.
-    await page.getByRole('link', { name: 'Sobre nosotros' }).click();
-  
-    // Expects the URL to contain intro.
-    await expect(page).toHaveURL(/.*about/);
-  });*/
+ 
--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -4,5 +4,7 @@
         <a href="/tourism"> Estadisticas de turismo </a>
         <a href="/graphs"> Gráfico </a>
         <a href="/integrations">Usos e Integraciones</a>
+        <a href="https://youtu.be/jm3bi5mCnC8">About</a>
+
     </div>
 </nav>
